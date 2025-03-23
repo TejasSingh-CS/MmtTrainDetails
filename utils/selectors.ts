8b8c43f5e6153562905414178db6db781cc31ee7
Tejas Singh <tejas.singh@confidosoft.co.uk>
2025-03-23 10:09:33 +0530

    Name changed

---
+++ utils/selectors.ts
@@ -0,0 +1,16 @@
+export const Selectors = {
+    MODAL_CLOSE: "//span[@class='commonModal__close']",
+    TRAINS_MENU: "//li[@class='menu_Trains']//span[@data-cy='item-wrapper']",
+    FROM_INPUT: "//span[normalize-space()='From']",
+    TO_INPUT: "//span[normalize-space()='To']",
+    DATE_INPUT: "//input[@id='travelDate']",
+    SUBMIT_BUTTON: "//a[@data-cy='submit']",
+    TRAIN_CARD: "[data-testid='listing-card']",
+    TRAIN_NAME: "[data-testid='train-name']",
+    TRAIN_NUMBER: "[data-testid='listing-train-number']",
+    DEPARTURE_CITY: "[data-testid='departure-city']",
+    ARRIVAL_CITY: "[data-testid='arrival-city']",
+    CLASS_CARD: "[data-testid='card-wrapper']",
+    CLASS_INFO: "[data-testid='class-info']",
+    AVAILABILITY_INFO: "[data-testid='availability-text']"
+  };
